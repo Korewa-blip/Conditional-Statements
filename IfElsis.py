19afaa5768f5aa98b1a6c77ace1d112df4ba5dca
Korewa-blip <murekosh@gmail.com>
2025-07-27 18:46:54 +0100

    Add files via upload

---
+++ IfElsis.py
@@ -0,0 +1,15 @@
+a=8
+b=10
+c=0
+if a and b and c:
+ print("All the numbers have boolean value as true")
+else:
+ print("One number has boolean value as false")  
+if a or b or c:
+ print("All the numbers have boolean value as true")
+else:
+ print("One number has boolean value as false")
+if not c:
+ print("True")
+else:
+ print("False")   
